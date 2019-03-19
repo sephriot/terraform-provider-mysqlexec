abea49e9120fd969c23a435a9f110549a721dde4
Jakub Wojtowicz <jakub.wojtowicz@tomtom.com>
2019-03-19 11:16:00 +0100

    Added DNS name resolving before connection
    Signed-off-by: Jakub Wojtowicz <jakub.wojtowicz@tomtom.com>

--- mysqlexec/client.go
+++ mysqlexec/client.go
@@ -3,6 +3,9 @@ package mysqlexec
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"net"
+	"strings"
+	"time"
 )
 
 type mySqlDbClient struct {
@@ -12,8 +15,34 @@ type mySqlDbClient struct {
 	password string
 }
 
+func (client *mySqlDbClient) lookupIP() (net.IP, error) {
+
+	maxRetries := 30
+	retries := 0
+	host := client.endpoint
+	if strings.Contains(host, "tcp(") && strings.Contains(host, ")") {
+		host = host[strings.Index(host, "tcp(")+4:]
+		host = strings.Replace(host, ")", "", 1)
+	}
+	host = host[0:strings.Index(host, "/")]
+	ips, err := net.LookupIP(host)
+	for ; err != nil && retries < maxRetries; retries++ {
+		time.Sleep(time.Duration(10) * time.Second)
+		ips, err = net.LookupIP(host)
+	}
+	var ip net.IP
+	if err == nil {
+		ip = ips[0]
+	}
+	return ip, err
+}
+
 func (client *mySqlDbClient) open() error {
 
+	_, err := client.lookupIP()
+	if err != nil {
+		return err
+	}
 	db, err := sql.Open("mysql", client.username + ":" + client.password + "@" + client.endpoint)
 	if err != nil {
 		return err
